b9eb0636120bc5464321687b4db71417bbd1cad2
Gavin Youker <youkergav@gmail.com>
2021-11-23 19:16:27 -0500

    Added unit testing for args

--- malbench/args.py
+++ malbench/args.py
@@ -3,17 +3,23 @@ import stat
 import argparse
 
 class Args:
-    def __new__(cls):
+    def __new__(cls, commands=[]):
         parser = argparse.ArgumentParser()
 
         parser.add_argument("path", type=Args.__get_path__, help="file or folder path to the malware samples")
         parser.add_argument("-v", "--verbose", action="store_true", help="prints additional info")
         parser.add_argument("--no-banner", action="store_true", help="hides the banner logo")
         
+        if commands:
+            return parser.parse_args(commands)
+            
         return parser.parse_args()
 
     def __is_executable__(path):
-        return stat.S_IXUSR & os.stat(path)[stat.ST_MODE]
+        if stat.S_IXUSR & os.stat(path)[stat.ST_MODE]:
+            return True
+
+        return False
 
     def __get_path__(path):
         files = []
---
+++ tests/test_args.py
@@ -0,0 +1,49 @@
+import os
+import unittest
+from unittest.mock import patch
+import malbench.args as args
+
+class TestArgs(unittest.TestCase):
+    def setUp(self):
+        self.folderpath = "tests/data/samples/"
+
+    def test_args(self):
+        # Test filepath
+        results = args.Args([self.folderpath + "calc.exe"])
+        self.assertListEqual(results.path, [os.path.abspath("tests/data/samples/calc.exe")])
+
+        # Test folderpath
+        results = args.Args([self.folderpath])
+        test = [
+            os.path.abspath("tests/data/samples/calc.exe"),
+            os.path.abspath("tests/data/samples/notepad.exe"),
+        ]
+        self.assertListEqual(results.path, test)
+
+        # Test verbose flag
+        results = args.Args([self.folderpath + "calc.exe", "--verbose"])
+        self.assertTrue(results.verbose)
+
+        # Test no-banner flag
+        results = args.Args([self.folderpath + "calc.exe", "--no-banner"])
+        self.assertTrue(results.no_banner)
+
+        # Test no arguments
+        with self.assertRaises(SystemExit) as results:
+            args.Args()
+        self.assertEqual(results.exception.code, 2)
+
+    def test_is_exectuable(self):
+        self.assertTrue(args.Args.__is_executable__(self.folderpath + "calc.exe"))
+
+    def test_get_path(self):
+        # Test filepath
+        self.assertListEqual(args.Args.__get_path__(self.folderpath + "calc.exe"), [os.path.abspath("tests/data/samples/calc.exe")])
+
+        # Test folderpath
+        test = [
+            os.path.abspath("tests/data/samples/calc.exe"),
+            os.path.abspath("tests/data/samples/notepad.exe"),
+        ]
+
+        self.assertListEqual(args.Args.__get_path__(self.folderpath), test)
