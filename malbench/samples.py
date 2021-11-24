ac906c1a9b4660242c8271578178a45da3630f16
Gavin Youker <youkergav@gmail.com>
2021-11-23 20:07:32 -0500

    Added unit testing for samples

--- malbench/samples.py
+++ malbench/samples.py
@@ -3,11 +3,13 @@ import time
 import subprocess
 
 class Sample:
-    def __init__(self, filepath, args=[], autorun=True):
+    def __init__(self, filepath, args=[], autorun=True, timeout=2):
         self.filepath = filepath
         self.args = args
         self.name = self.__get_name__(self.filepath)
-        self.timestamp = time.time()
+        self.timestamp = 0
+        self.timeout = timeout
+        self.process = None
         self.detected = None
 
         if autorun:
@@ -22,9 +24,11 @@ class Sample:
 
     def start(self):
         command = [self.filepath] + self.args
+        self.timestamp = time. time()
 
         try:
-            return subprocess.Popen(command, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP | subprocess.CREATE_BREAKAWAY_FROM_JOB)
+            self.process = subprocess.Popen(command, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP | subprocess.CREATE_BREAKAWAY_FROM_JOB)
+            return self.process
         except WindowsError as error:
             # Skip process monitoring if the file was blocked on startup for malware detection.
             if error.winerror == 225:
@@ -36,7 +40,7 @@ class Sample:
     def monitor(self):
         return_code = self.process.poll()
 
-        if time.time() - self.timestamp > 2:
+        if time.time() - self.timestamp > self.timeout:
             return self.__update_detected__(False)
         elif return_code != None:
             if return_code != 0:
---
+++ tests/test_samples.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import time
+import unittest
+from unittest.mock import patch
+import malbench.samples as samples
+
+class TestSamples(unittest.TestCase):
+    def setUp(self):
+        self.sample = samples.Sample(os.path.abspath("tests/data/samples/notepad.exe"), autorun=False)
+
+    def test_get_name(self):
+        self.assertEqual(self.sample.__get_name__(self.sample.filepath), "notepad.exe")
+
+    def test_update_detected(self):
+        # Test updating True
+        self.assertTrue(self.sample.__update_detected__(True))
+        self.assertEqual(self.sample.detected, True)
+
+        # Test updating False
+        self.assertFalse(self.sample.__update_detected__(False))
+        self.assertEqual(self.sample.detected, False)
+
+    def test_start(self):
+        result = self.sample.start()
+        self.assertIsInstance(result, subprocess.Popen)
+        self.assertGreater(self.sample.process.pid, 0)
+        self.assertNotEqual(self.sample.process.returncode, 0)
+
+    def test_monitor(self):
+        if self.sample.process:
+            time.sleep(self.sample.timeout + 1)
+
+            self.assertFalse(self.sample.monitor())
+            self.assertFalse(self.sample.detected)
